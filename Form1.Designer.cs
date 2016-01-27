b2050e839f0410a8d525739bcdeb9b582479d160
Piotr Szura <pioszura@live.com>
2016-01-27 19:49:47 +0100

    pobieranie obrazu z kamerki i wyswietlanie go

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pbObraz1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbObraz1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pbObraz1
+            // 
+            this.pbObraz1.Location = new System.Drawing.Point(12, 12);
+            this.pbObraz1.Name = "pbObraz1";
+            this.pbObraz1.Size = new System.Drawing.Size(320, 240);
+            this.pbObraz1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbObraz1.TabIndex = 0;
+            this.pbObraz1.TabStop = false;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(1125, 479);
+            this.Controls.Add(this.pbObraz1);
             this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "NAI Morse project";
             this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbObraz1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbObraz1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,19 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Util;
+using Emgu.CV.Structure;
+using System.Threading;
 
 namespace naiMorse
 {
     public partial class frmMain : Form
     {
+        Thread th_pobierzObraz; //wątek do odczytu kamerki i przetwarzania obrazu
+        Capture kamerka;
+        Image<Bgr, Byte> obraz1; //oryginalny obraz z kamerki
+
         public frmMain()
         {
             InitializeComponent();
+            //inicjalizacja kamerki
+            kamerka = new Capture(0);
+            //wątek odczytu start
+            th_pobierzObraz = new Thread(pobierzObraz);
+            th_pobierzObraz.Start();
+        }
+
+        void pobierzObraz() //funkcja z nieskonczoną pętlą, działa w wątku th_pobierzObraz
+        {
+            for(;;)
+            {
+                //oryginalny obraz                
+                obraz1 = new Image<Bgr, byte>(new Size(640, 480));
+                obraz1 = kamerka.QueryFrame();
+
+                pbObraz1.Image = obraz1.Bitmap;
+
+                Thread.Sleep(10);
+            }
         }
 
         private void frmMain_Load(object sender, EventArgs e)
         {
-
+         
         }
     }
 }
